bd37dd7d14ad8b478adf861f705d6c8d5198e537
shushmek <295andrey@gmail.com>
2026-04-06 21:53:09 +0300

    Update Engine.h
    добавил коментарии

--- Engine.h
+++ Engine.h
@@ -11,31 +11,31 @@ using namespace std;
 
 class Engine
 {
-	AssetManager manager;
-	unique_ptr<RenderWindow>  window = make_unique<RenderWindow>(VideoMode({ 1280, 720 }), "SFML works!", Style::Default);
-	RectangleShape background = RectangleShape({ 1280, 720 });
-	///////////////////////////////////////////////////////////////// BUTTON FOR TEST
-	Button btn = Button({ 700,200 }, "Sprite/square.jpg", "START", "Font/BankGothic.otf", "", Color::Black, 35);
-	Button exitBTN = Button({ 700,400 }, "Sprite/square.jpg", "EXIT", "Font/BankGothic.otf", "", Color::Black, 35);
-	Button closeBTN = Button({ 50,50 }, "Sprite/square.jpg", "CL0SE", "Font/BankGothic.otf");
-	RectangleShape scene = RectangleShape({ 1280, 720 });
-	bool sceneActiv = false;
+	unique_ptr<RenderWindow>  window = make_unique<RenderWindow>(VideoMode({ 1280, 720 }), "SFML works!", Style::Default); //указатель на окно
+	RectangleShape background = RectangleShape({ 1280, 720 }); // обьявление фона
+	///////////////////////////////////////////////////////////////// демонстрация ∨∨∨
+	// создвание кнопок
+	Button btn = Button({ 700,200 }, "Sprite/square.jpg", "START", "Font/BankGothic.otf", "", Color::Black, 35); //переход на "другую" сцену(просто отрисовка нового фона поверх других)
+	Button exitBTN = Button({ 700,400 }, "Sprite/square.jpg", "EXIT", "Font/BankGothic.otf", "", Color::Black, 35); //выход из игры
+	Button closeBTN = Button({ 50,50 }, "Sprite/square.jpg", "CL0SE", "Font/BankGothic.otf"); //вернутся в первую сцену
+	Button P = Button({ 300,500 }, "Sprite/square.jpg", "Pause", "Font/BankGothic.otf"); // пауза(для теста анимации)
+	Button R = Button({ 600,500 }, "Sprite/square.jpg", "restart", "Font/BankGothic.otf"); //рестарт(для теста анимации)
+	
+	//вторая сцена для наглядности работы кнопок
+	RectangleShape scene = RectangleShape({ 1280, 720 }); //задник сцены
+	bool sceneActiv = false;//состояние для сцены(вкл\выкл)
 
-	Button P = Button({ 300,500 }, "Sprite/square.jpg", "Pause", "Font/BankGothic.otf");
-	Button R = Button({ 600,500 }, "Sprite/square.jpg", "restart", "Font/BankGothic.otf");
+	Texture fireTexture = AssetManager::GetTexture("Sprite/fire.png"); //текстура спрайтшита(спрайт с кадрами для аниамции) для огня
+	Sprite* fire; //спрайт огня
+	Animator* fireAnim;//агимация огня
+	///////////////////////////////////////////////////////////////// демонстрация ∧∧∧
 
-	Texture fireTexture = AssetManager::GetTexture("Sprite/fire.png");
-	Sprite* fire;
-	Animator* fireAnim;// = Animator(*fire, 100);
-	///////////////////////////////////////////////////////////////// 
-
-	void Input();
-	void Update(Time const& deltaTime);
-	void Draw();
+	void Input(); // обработка нажатий и ввода с клавиатуры
+	void Update(Time const& deltaTime); // цикл наподобие while(true), deltaTime - это время, тут писать логику зависящую от времени
+	void Draw();//ортисовка объектов, всякое говно не связанное с отрисовкой не пихать
 public:
-	Engine();
+	Engine(); //конструктор, ничего не требует
 
-	void Run();
-
+	void Run(); // функция котораязапускает прокгамму в ней можно обьявлять переменные и т.д.
 };
 
