c754c44450b43f448c3c942667b7b8df0c228a54
Shmek <Shmek@DESKTOP-3T06CV6>
2026-04-30 19:22:55 +0300

    добавленны базовые переменные

--- Weapon.h
+++ Weapon.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
+#include<SFML/Graphics.hpp>
+#include "Animator.h"
+#include "Object.h"
+#include "Cards.h"
+#include "Effects.h"
+
+using namespace sf;
+using namespace std;
+
 class Weapon
 {
+	enum Rarity {Common, Rare, Epick, Mythic, Legendary};
+
+	string name;
+	Rarity _rarity;
+	int _damage;
+	int _minBet;
+	int _maxBet;
+	int _move;
+	int _combo;
+	Effects _effect;
 };
 
