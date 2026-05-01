37e8866ec43114581ca64272f23dd1fa3e627c44
Shmek <Shmek@DESKTOP-3T06CV6>
2026-05-01 19:21:17 +0300

    сделаны классы Weapon, Armor

--- Weapon.h
+++ Weapon.h
@@ -1,25 +1,37 @@
 #pragma once
 #include <iostream>
 #include<SFML/Graphics.hpp>
-#include "Animator.h"
 #include "Object.h"
 #include "Cards.h"
 #include "Effects.h"
+#include "Creature.h"
 
 using namespace sf;
 using namespace std;
 
 class Weapon
 {
-	enum Rarity {Common, Rare, Epick, Mythic, Legendary};
-
-	string name;
+	enum Rarity {None, Common, Rare, Epick, Mythic, Legendary};
+public:
+	Weapon(const string& name, const Rarity& rarity, const int& damage, const int& min, const int& max, const int& move, const int& combo, Texture const& texture, IntRect const& rect, int frameCount, int Ycount, int frameTime = 0, float animTime = 0);
+	void TakeDamage(Creature target, int damage);
+	int getDamage();
+	string getName();
+	int getRarity();
+	int getMinBet();
+	int getMaxBet();
+	int getMove();
+	int getCombo();
+	//Effects getEffect();
+private:
+	string _name;
 	Rarity _rarity;
 	int _damage;
 	int _minBet;
 	int _maxBet;
 	int _move;
 	int _combo;
-	Effects _effect;
+	//Effects _effect;
+	Object* _sWeapon;
 };
 
